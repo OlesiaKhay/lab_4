96d39d0359ac8df58c89ebc636fcc2830a48095d
OlesiaKhay <148855170+OlesiaKhay@users.noreply.github.com>
2023-11-25 22:01:11 +0300

    Add files via upload

---
+++ lab 4/найти сумму произведений из списка словарей.py
@@ -0,0 +1,13 @@
+# TODO решите задачу
+def task() -> float:
+    import json
+    with open ("input.json", "r") as file:
+        data = json.load(file)
+        file.close()
+    res = []
+    for elem in data: res.append(elem['score']*elem['weight'])
+    return round(sum(res), 3)
+
+
+
+print(task())

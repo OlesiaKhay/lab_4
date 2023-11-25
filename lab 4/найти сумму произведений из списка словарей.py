5ba3f38e03586ac15c919fbe0d34f9e7313da857
OlesiaKhay <148855170+OlesiaKhay@users.noreply.github.com>
2023-11-25 22:06:08 +0300

    найти сумму произведений из списка словарей.py

--- lab 4/найти сумму произведений из списка словарей.py
+++ lab 4/найти сумму произведений из списка словарей.py
@@ -3,7 +3,6 @@ def task() -> float:
     import json
     with open ("input.json", "r") as file:
         data = json.load(file)
-        file.close()
     res = []
     for elem in data: res.append(elem['score']*elem['weight'])
     return round(sum(res), 3)
